17dd62a811d0a5c828eab5485564d00ac9dd72b0
Andrey <web__master@list.ru>
2022-08-19 00:27:44 +0500

    :)

--- LessonOne.java
+++ LessonOne.java
@@ -1,3 +1,8 @@
+public class LessonOne {
+        public static void main(String[] args) {
+                System.out.print("Hello world");
+        }
+}
 // import java.util.Scanner;
 
 // LessonOne - коментарии
